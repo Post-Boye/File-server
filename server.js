76033b097ce490500c2233b8c31e6bf285bd77ba
Boye <boyejustice64@gmail.com>
2024-04-01 17:36:08 +0000

    add more

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const http = httpObj.createServer(app);
 
 const mainURL = "http://localhost:3000";
 
-const mongodb = require("mongodb");
-const mongoClient = mongodb.MongoClient;
-const ObjectId = mongodb.ObjectId;
+// const mongodb = require("mongodb");
+// const mongoClient = mongodb.MongoClient;
+// const ObjectId = mongodb.ObjectId;
 
 app.set("view engine", "ejs");
 
@@ -30,14 +30,34 @@ app.use(function (request, result, next){
     next();
 });
 
+// express formidable is used to parse the form data values
+const formidable = require("express-formidable");
+app.use(formidable());
+
+// to encrypt/decrypt passwords
+const bcrypt = require("bcrypt");
+const nodemailer = require("nodemailer");
+
+const nodemailerFrom = "boyejustice64@gmail.com";
+let nodemailerObject = {
+    service: "gmail",
+    host : "smtp.gmail.com",
+    port: 45,
+    secure : true,
+    auth : {
+        user : "boyejustice64@gmail.com",
+        pass : ""
+    }
+}
+
 http.listen(3000,function(){
     console.log("server started at " + mainURL);
 
-    mongoClient.connect("mongodb://localhost:27017",{
-        useNewUrlParser: true
-    }, function (error,client){
-        database = client.db("File_server");
-        console.log("Database connected.");
+    // mongoClient.connect("mongodb://localhost:27017",{
+    //     useNewUrlParser: true
+    // }, function (error,client){
+    //     database = client.db("File_server");
+    //     console.log("Database connected.");
 
         app.get("/", function(request, result){
             result.render("index",{
@@ -52,8 +72,23 @@ http.listen(3000,function(){
             });
         });
 
+        // register the user
+        app.post("/Register", async function (request, response) {
+            let name = request.fields.name;
+            let email = request.fields.email;
+            let password = request.fields.password;
+            let reset_token = "";
+            let isVerified = true;
+            let verification_token = new Date().getTime();
+        
+            let user = await database.collection("users").findOne({
+                "email": email
+            });
+        });
+        
+       
 
 
 
     });
-});
+;
