5c38d4c0cb56f6e1c3120a2e4425516bab8a1e85
Boye <boyejustice64@gmail.com>
2024-05-19 12:20:11 +0000

    hello world

--- server.js
+++ server.js
@@ -37,6 +37,7 @@ app.use(formidable());
 // to encrypt/decrypt passwords
 const bcrypt = require("bcrypt");
 const nodemailer = require("nodemailer");
+const { Http2ServerRequest } = require("http2");
 
 const nodemailerFrom = "boyejustice64@gmail.com";
 let nodemailerObject = {
@@ -84,11 +85,264 @@ http.listen(3000,function(){
             let user = await database.collection("users").findOne({
                 "email": email
             });
+
+            if (user == null) {
+                bcrypt.hash(password, 10, async function (error, hash) {
+                    await database.collection("users").insertOne({
+                        "name": name,
+                        "email": email,
+                        "password": hash,
+                        "reset_token": reset_token,
+                        "uploaded": [],
+                        "sharedWithMe": [],
+                        "isVerified": isVerified,
+                        "verification_token": verification_token
+                    }, async function (error, data) {
+
+                        let transporter = nodemailer.createTransport(
+                            nodemailerObject
+                        );
+
+                        let text = "Please verify your account by clicking on the link: " + mainURL + "/verifyEmail/" + email + "/" + verification_token;
+
+                        let html = "Please verify your account by clicking the following link: <br><br> <a href =" + mainURL + "/" + verification_token + ">confirm Email</a> <br></br> Thank You.";
+                        await transporter.sendMail({
+                            from: nodemailerFrom,
+                            to : email,
+                            subject: "email verification",
+                            text : text,
+                            html : html},
+                            function (error, info ) {
+                                if(error){
+                                    console.log(error);
+                                }else{
+                                        console.log("Email sent: " + info,
+                                        response);
+                                    }
+
+                                    request.status = "success";
+                                    request.message = "Signed up successfully. You can login now.";
+
+                                    result.render("Register", {
+                                        "request": request
+                                    });
+                                }
+                                
+                        )
+
+    
+                
+                    
+
+                        
+
+                        
+                        
+                    });
+                });
+            } else {
+                request.status = "error";
+                request.message = "Email already exist.";
+
+                result.render("Register", {
+                    "request": request
+                });
+            }
         });
+
+        app.get("/verifyEmail?:email/:email/:verification_token", async function (request, result){
+
+            let email = request.params.email;
+            let verification_token = request.params.verification_token;
+
+            let user = await database.collection("users").findOne({
+                $and: [{
+                    "email" : email,
+                }, { 
+                    "verification_token": parseInt(verification_token)
+                }]
+            });
+
+            if (user == null){
+                request.status = "error";
+                request.message = "Email does not exists. Or verification link expired";
+                result.render("login", {
+                    "request": request
+                })
+            }else{
+                await database.collection("users").findOneAndUpdate({
+                    $and: [{
+                        "email" : email,
+                    },{
+                        "verification_token" : parseInt( verification_token)
+                    }]
+                    }, {
+                        $set: {
+                            "verification_token": "",
+                            "isVerified": true
+                        }
+            
+                });
+
+                request.status = "success";
+                request.message = "Account has been verified. Please try login. "; {
+                    result.render("Login", {
+                        "request": request
+                    });
+
+                }
+            }
+
+
+        });
+
+        app.get("/Login", function(request,result){
+            result.render("Login", {
+                "request": request
+            });
+        });
+
+        app.post("/Login",async function(request, result){
+            let email = request.fields.email;
+            let password = request.fields.password;
+
+            let user = await database.collection("users").findOne({
+                "email": email
+            });
+
+            if (user == null){
+                request.status = "error";
+                request.message = "Email does not exist. ";
+
+                request.render("Login", {
+                    "request": request
+                });
+
+                return false;
+            }
+
+            bcrypt.compare(password, user.password, function(error, isVerify){
+                if (isVerify){
+                    if(user.isVerify){
+                        request.session.user = user;
+                        result.redirect("/");
+
+                        return false;
+                    }
+
+                    request.status = "error";
+                    request.message = "kindly verify your email.";
+                    result.render("Login", {
+                        "request": request
+                    });
+
+                    return false;
+                }
+
+                request.status = "error"
+                request.message = "Password is not correct.";
+                result.render("Login", {
+                    "request": request
+                });
+            }); 
+            app.post("/SendRecoveryLink", async function(request, result){
+
+                let email = request.fields.email;
+                let user = await database.collection(users).findOne({
+                    "email": email
+                });
+
+                if (user == null){
+                    request.status = "error"
+                    request.message = "Email does not exists.";
+
+                    result.render("ForgotPassword", {
+                        "request":request
+                    });
+                    return false;
+
+                }
+                let reset_token = new Date().getTime();
+
+                await database.collection("users").findOneAndUpdate({
+                    "email": email
+                }, {
+                    $set: {
+                        "reset_token": reset_token
+                    }
+                });
+
+                let transporter = nodemailer.createTransport(
+                    nodemailerObject
+                );
+
+                let text = "Please reset  your password by clicking on the link: " + mainURL + "/ResetPassword/" + email + "/" + reset_token;
+
+                let html = "Please verify your account by clicking the following link: <br><br> <a href =" + mainURL + "/ResetPassword/" + email + "/" + reset_token + ">reset password</a> <br></br> Thank You.";
+                await transporter.sendMail({
+                    from: nodemailerFrom,
+                    to : email,
+                    subject: "Reset Password",
+                    text : text,
+                    html : html},
+                    function (error, info ) {
+                        if(error){
+                            console.log(error);
+                        }else{
+                                console.log("Email sent: " + info,
+                                response);
+                            }
+
+                            request.status = "success";
+                            request.message = "Email has been sent with the link to recover the password.";
+
+                            result.render("ForgotPassword", {
+                                "request": request 
+                            });
+
+
+
+            });
+        });
+        app.get("/ResetPassword/:email/:reset_token",async function(request,response){
+            let email = request.params.email;
+            let reset_token = request.params.reset_token;
+
+            let user = await database.collection("users").findOne({
+                $and: [{
+                    "email": email
+                }, {
+                    "reset_token": parseInt(reset_token)
+                }]
+            });
+
+            if (user == null){
+                request.status = "error";
+                request.message = "Link is expired.";
+                result.render("Error",{
+                    "request": request
+                });
+                return false;
+            }
+
+            result.render("ResetPassword",{
+                "request": request,
+                "email": email,
+                "reset_token": reset_token
+            });
+
+
+        });
+
+
+
+     });
+
+
         
        
 
 
 
-    });
-;
+    
+});
